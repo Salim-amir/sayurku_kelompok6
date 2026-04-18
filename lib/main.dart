a206e3883ba3d39fa5bb9577b3908b1bf740b7d1
Salim-amir <salimamir2005@gmail.com>
2026-04-19 01:20:22 +0700

    Setup: Inisialisasi Firebase Core dan Konfigurasi Environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; 
+import 'firebase_options.dart'; 
 import 'core/colors.dart';
-// Import kedua halaman auth kamu
 import 'features/auth/login_screen.dart';
 import 'features/auth/splash_screen.dart';
 
-void main() {
-  // Jika nanti sudah pakai Firebase, inisialisasi dilakukan di sini
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
